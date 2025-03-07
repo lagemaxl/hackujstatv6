5193172cdaa7494705c9676c79a25114b618de50
Ladislav Pokorný <ladislav.pokorny@portabo.cz>
2025-03-07 14:06:51 +0100

    Add Leaflet integration for map functionality and update dependencies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,27 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import "./style/Home.css";
+
 const Home = () => {
-    return <h1>Home</h1>;
-  };
-  
-  export default Home;
+  return (
+    <div>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        className="map"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Home;
