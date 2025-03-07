946a1c999aeec6a3dd5d17e86aaa2812820b780c
Ladislav Pokorný <ladislav.pokorny@portabo.cz>
2025-03-07 23:43:03 +0100

    Refactor Home component to optimize district loading; prevent duplicate fetches and enhance map interaction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,119 +1,137 @@
-import {
-    MapContainer,
-    TileLayer,
-    GeoJSON,
-    useMap,
-  } from "react-leaflet";
-  import { useState, useEffect } from "react";
-  import "./style/Home.css";
-  import L from "leaflet";
-  import { IconArrowLeft } from "@tabler/icons-react";
-  
-  const Home = () => {
-    const [geojsonData, setGeojsonData] = useState(null);
-    const [districtsGeojsonData, setDistrictsGeojsonData] = useState(null);
-    const [selectedRegion, setSelectedRegion] = useState(null);
-  
-    useEffect(() => {
-      fetch(`kraje.json`)
-        .then((response) => response.json())
-        .then((data) => {
-          setGeojsonData(data);
-        });
-    }, []);
-  
-    const loadDistricts = async (region) => {
-        try {
-          const nationalCode = region.nationalCode;
-      
-          if (!nationalCode) {
-            console.error("National code not found for the selected region.");
-            return;
-          }
-      
-          const response = await fetch(`${import.meta.env.VITE_API_URL}/kraje/${nationalCode}/okresy`, {
-            headers: { accept: "application/json" },
-          });
-          const apiDistricts = await response.json();
+import { MapContainer, TileLayer, GeoJSON, useMap } from "react-leaflet";
+import { useState, useEffect, useRef } from "react";
+import "./style/Home.css";
+import L from "leaflet";
+import { IconArrowLeft } from "@tabler/icons-react";
 
-          const localResponse = await fetch(`okresy.json`);
-          const localDistrictsData = await localResponse.json();
+const Home = () => {
+  const [geojsonData, setGeojsonData] = useState(null);
+  const [districtsGeojsonData, setDistrictsGeojsonData] = useState(null);
+  const [selectedRegion, setSelectedRegion] = useState(null);
+  const [lastFetchedRegion, setLastFetchedRegion] = useState(null); // Ukládá poslední fetchnutý region
 
-          const validNationalCodes = apiDistricts.map((district) => district.kod.toString());
+  useEffect(() => {
+    fetch(`kraje.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        setGeojsonData(data);
+      });
+  }, []);
 
-          const filteredDistricts = {
-            type: "FeatureCollection",
-            features: localDistrictsData.features.filter((feature) =>
-              validNationalCodes.includes(feature.nationalCode)
-            ),
-          };
-      
-          setDistrictsGeojsonData(filteredDistricts);
-        } catch (error) {
-          console.error("Error loading districts:", error);
+  const loadDistricts = async (region) => {
+    if (!region || region === lastFetchedRegion) return; // Fetchujeme pouze pokud je nový region
+
+    try {
+      const nationalCode = region.nationalCode;
+
+      if (!nationalCode) {
+        console.error("National code not found for the selected region.");
+        return;
+      }
+
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/kraje/${nationalCode}/okresy`,
+        {
+          headers: { accept: "application/json" },
         }
+      );
+      const apiDistricts = await response.json();
+
+      const localResponse = await fetch(`okresy.json`);
+      const localDistrictsData = await localResponse.json();
+
+      const validNationalCodes = apiDistricts.map((district) =>
+        district.kod.toString()
+      );
+
+      const filteredDistricts = {
+        type: "FeatureCollection",
+        features: localDistrictsData.features.filter((feature) =>
+          validNationalCodes.includes(feature.nationalCode)
+        ),
       };
-      
-  
-    return (
-      <div>
-        <MapContainer
-          center={[49.7437572, 15.3386383]}
-          zoom={8}
-          scrollWheelZoom={true}
-          className="map"
-        >
-          {/* kraje */}
-          {geojsonData && !selectedRegion && (
-            <GeoJSON
-              data={geojsonData}
-              style={{
-                fillColor: "var(--secondary)",
-                fillOpacity: 0.3,
-                color: "black",
-                weight: 1.5,
-              }}
-              eventHandlers={{
-                click: (e) => {
-                  const region = e.layer.feature;
-                  setSelectedRegion(region);
-                  loadDistricts(region); // Načtení okresů pro vybraný kraj
-                },
-              }}
-            />
-          )}
-  
-          {/* okresy */}
-          {selectedRegion && (
-            <ZoomToRegion
-              region={selectedRegion}
-              districts={districtsGeojsonData}
-              onReset={() => {
-                setSelectedRegion(null);
-                setDistrictsGeojsonData(null);
-              }}
-            />
-          )}
-  
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
-      </div>
-    );
+
+      setDistrictsGeojsonData(filteredDistricts);
+      setLastFetchedRegion(region); // Uložíme, že už jsme okresy pro tento kraj načetli
+    } catch (error) {
+      console.error("Error loading districts:", error);
+    }
   };
-  
-  const ZoomToRegion = ({ region, districts, onReset }) => {
+
+  return (
+    <div>
+      <MapContainer
+        center={[49.7437572, 15.3386383]}
+        zoom={8}
+        scrollWheelZoom={true}
+        className="map"
+      >
+        {/* kraje */}
+        {geojsonData && !selectedRegion && (
+          <GeoJSON
+            data={geojsonData}
+            style={{
+              fillColor: "var(--secondary)",
+              fillOpacity: 0.3,
+              color: "black",
+              weight: 1.5,
+            }}
+            eventHandlers={{
+              click: (e) => {
+                const region = e.layer.feature;
+                setSelectedRegion(region);
+              },
+            }}
+          />
+        )}
+
+        {/* okresy */}
+        {selectedRegion && (
+          <ZoomToRegion
+            region={selectedRegion}
+            districts={districtsGeojsonData}
+            onReset={() => {
+              setSelectedRegion(null);
+              setDistrictsGeojsonData(null);
+              setLastFetchedRegion(null);
+            }}
+            onFlyEnd={loadDistricts} // Předáme funkci, která fetchne okresy jen jednou
+          />
+        )}
+
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+};
+
+const ZoomToRegion = ({ region, districts, onReset, onFlyEnd }) => {
     const map = useMap();
+    const moveEndRef = useRef(false); // Použití useRef k zabránění duplicitního volání
   
     useEffect(() => {
       if (region) {
         const bounds = L.geoJSON(region.geometry).getBounds();
         const center = bounds.getCenter();
+        
         map.flyTo(center, 9, { duration: 0.5 });
+  
+        if (!moveEndRef.current) {  // Zabráníme opakovanému přidání listeneru
+          moveEndRef.current = true;
+  
+          const handleMoveEnd = () => {
+            onFlyEnd(region); // Zavoláme fetch okresů
+            map.off("moveend", handleMoveEnd); // Odebereme event listener
+            moveEndRef.current = false; // Resetujeme flag pro další interakci
+          };
+  
+          map.on("moveend", handleMoveEnd);
+        }
       }
-    }, [region, map]);
+    }, [region, map, onFlyEnd]);
   
     const zoomToRepublic = () => {
       map.setView([49.7437572, 15.3386383], 8, { duration: 0.5 });
@@ -138,7 +156,7 @@ import {
           <GeoJSON
             data={districts}
             style={{
-              fillColor: "#f44336",
+              fillColor: "var(--secondary)",
               fillOpacity: 0.3,
               color: "black",
               weight: 1,
@@ -153,5 +171,4 @@ import {
     );
   };
   
-  export default Home;
-  
+export default Home;
