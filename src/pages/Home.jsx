37677587d4c3aa55854f7896a49dd3a67ad17e68
Ladislav Pokorný <ladislav.pokorny@portabo.cz>
2025-03-07 14:20:56 +0100

    Add GeoJSON support and update map center and zoom level

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,48 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from "react-leaflet";
+import { useState, useEffect } from "react";
 import "./style/Home.css";
 
 const Home = () => {
+  const [geojsonData, setGeojsonData] = useState(null);
+
+  const geoJSONStyle = (feature) => {
+    return {
+      fillColor: "#1976d2",
+      fillOpacity: 0.1,
+      color: "black",
+      weight: 1.5,
+    };
+  };
+
+  useEffect(() => {
+    setGeojsonData(null);
+    fetch(`kraje.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        setGeojsonData(data);
+      });
+  }, []);
+
   return (
     <div>
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
+        center={[49.7437572, 15.3386383]}
+        zoom={8}
+        scrollWheelZoom={true}
         className="map"
       >
+        {geojsonData && (
+          <GeoJSON
+            data={geojsonData}
+            style={geoJSONStyle}
+            eventHandlers={{
+            }}
+          />
+        )}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
       </MapContainer>
     </div>
   );
