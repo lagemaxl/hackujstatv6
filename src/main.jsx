cae161cd224bdbb1d8418d28c9a654b0d939e8e7
Ladislav Pokorný <ladislav.pokorny@portabo.cz>
2025-03-07 18:12:05 +0100

    Add theme styles and enhance Home component with region selection functionality

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
+import "./pages/style/Theme.css";
 
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,97 @@
-import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from "react-leaflet";
-import { useState, useEffect } from "react";
-import "./style/Home.css";
-
-const Home = () => {
-  const [geojsonData, setGeojsonData] = useState(null);
-
-  const geoJSONStyle = (feature) => {
-    return {
-      fillColor: "#1976d2",
-      fillOpacity: 0.1,
-      color: "black",
-      weight: 1.5,
-    };
-  };
-
-  useEffect(() => {
-    setGeojsonData(null);
-    fetch(`kraje.json`)
-      .then((response) => response.json())
-      .then((data) => {
-        setGeojsonData(data);
-      });
-  }, []);
-
-  return (
-    <div>
-      <MapContainer
-        center={[49.7437572, 15.3386383]}
-        zoom={8}
-        scrollWheelZoom={true}
-        className="map"
-      >
-        {geojsonData && (
-          <GeoJSON
-            data={geojsonData}
-            style={geoJSONStyle}
-            eventHandlers={{
-              click: (e) => console.log(e.layer.feature.nationalCode),
-            }}
+import {
+    MapContainer,
+    TileLayer,
+    GeoJSON,
+    Marker,
+    Popup,
+    useMap,
+  } from "react-leaflet";
+  import { useState, useEffect } from "react";
+  import "./style/Home.css";
+  import L from "leaflet";
+  
+  const Home = () => {
+    const [geojsonData, setGeojsonData] = useState(null);
+    const [selectedRegion, setSelectedRegion] = useState(null);
+  
+    useEffect(() => {
+      fetch(`kraje.json`)
+        .then((response) => response.json())
+        .then((data) => {
+          setGeojsonData(data);
+        });
+    }, []);
+  
+    return (
+      <div>
+        <MapContainer
+          center={[49.7437572, 15.3386383]}
+          zoom={8}
+          scrollWheelZoom={true}
+          className="map"
+        >
+          {geojsonData && !selectedRegion && (
+            <GeoJSON
+              data={geojsonData}
+              style={{
+                fillColor: "#1976d2",
+                fillOpacity: 0.3,
+                color: "black",
+                weight: 1.5,
+              }}
+              eventHandlers={{
+                click: (e) => setSelectedRegion(e.layer.feature),
+              }}
+            />
+          )}
+  
+          {selectedRegion && <ZoomToRegion region={selectedRegion} onReset={() => setSelectedRegion(null)} />}
+  
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-        )}
-
-        <Marker position={[49.7437572, 15.3386383]}>
-          <Popup>Česká republika</Popup>
-        </Marker>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        </MapContainer>
+      </div>
+    );
+  };
+  
+  const ZoomToRegion = ({ region, onReset }) => {
+    const map = useMap();
+  
+    useEffect(() => {
+      if (region) {
+        const bounds = L.geoJSON(region.geometry).getBounds();
+        const center = bounds.getCenter();
+        map.flyTo(center, 9, { duration: 0.5 });
+      }
+    }, [region, map]);
+    
+    const zoomToRepublic = () => {
+      map.setView([49.7437572, 15.3386383], 8, { duration: 0.5 });
+      onReset(); 
+    };
+  
+    return (
+      <>
+        <GeoJSON
+          data={region}
+          style={{
+            fillColor: "#1976d2",
+            fillOpacity: 0.2,
+            color: "black",
+            weight: 2,
+          }}
         />
-      </MapContainer>
-    </div>
-  );
-};
-
-export default Home;
+        <button
+          onClick={zoomToRepublic}
+          className="zoom-back-button"
+        >
+          Zpět
+        </button>
+      </>
+    );
+  };
+  
+  export default Home;
+  
